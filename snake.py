96def4e37ff5e9313c0a221991fc215b667002b6
csgellert <csapodi.gellert@gmail.com>
2020-06-19 18:35:54 +0200

    Fix pygame window

--- snake.py
+++ snake.py
@@ -1,13 +1,13 @@
 from osztalyok import *   # minden osztályt / függvényt az osztalyok csomagból importálunk
-
+from tqdm import tqdm
 #main()
 #mozgott = False    # nem tudjuk mit csinál, lehet hogy még később kell
 ai = evol() # kezdő generáció
 genszam = 50
-for i in range(genszam):
+for i in tqdm(range(genszam)):
     ai.play()
     ai.fejlodes()
     ai = newgen(ai) # következő gen
-    print(i)
-ai.play(0) #Ha ezt átírjátok 1 re akkor megjeleníti a játékmenetet is
+    #print(i)
+ai.play() #Ha ezt átírjátok True ra akkor megjeleníti a játékmenetet is
 ai.fejlodes(True)
